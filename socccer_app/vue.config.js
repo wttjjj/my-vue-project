7ce6e1655c6e8b2d2c448102035e4330c137633b
王婷婷 <wangtingting@192.168.0.5>
2025-04-15 22:37:39 +0800

    部署

--- socccer_app/vue.config.js
+++ socccer_app/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/my-vue-project/socccer_app' 
+    : '/'
 })
